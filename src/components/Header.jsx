ee73c61fdfdc43f52913f1d51d8f3e9820477b56
FilipRosquist <filip.rosquist@gritacademy.se>
2025-04-15 16:36:40 +0200

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,19 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex item-center w-full overflow-hidden' style={{backgroundImage: "url('/header.jpg')"}} id='Header'>
-        <Navbar/>
-      
+    <div
+      className="min-h-screen mb-4 bg-cover bg-center w-full overflow-hidden"
+      style={{ backgroundImage: "url('/header.jpg')" }}
+      id="Header"
+    >
+      <Navbar />
+      <div className="flex flex-col items-center justify-center text-center min-h-screen px-6 text-white">
+        <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">Explore the life with Solar Panels</h2>
+        <div className="space-x-6 mt-16">
+          <a href="#Products" className="border border-white px-8 py-3 rounded">Products</a>
+          <a href="#Contact" className="bg-blue-800 px-8 py-3 roundede">Contact Us</a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
       <div className='container mx-auto flex justify-between item-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
         <img src={assets.logoSmall} alt="logo" />
         <ul className='hidden md:flex gap-7 text-white'>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-            <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
-            <a href="#Products" className='cursor-pointer hover:text-gray-400'>Products</a>
-            <a href="#Contact" className='cursor-pointer hover:text-gray-400'>Contact</a>
+            <a href="#Header" className='cursor-pointer hover:text-blue-800'>Home</a>
+            <a href="#About" className='cursor-pointer hover:text-blue-800'>About</a>
+            <a href="#Products" className='cursor-pointer hover:text-blue-800'>Products</a>
+            <a href="#Contact" className='cursor-pointer hover:text-blue-800'>Contact</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
       </div>
