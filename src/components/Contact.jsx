6507af87499aba3d52e204b6a8bee4448ef3dd6a
FilipRosquist <filip.rosquist@gritacademy.se>
2025-05-02 09:11:02 +0200

    1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
         <h1 className='text-2xl sm:text-4xl font font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>Us!</span></h1>
-        <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready For A Greener Future? Lets Build A More Sustainable World Together</p>
+        <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready For A Greener Future? <br/> Send Us An Email!</p>
 
         <form>
             <div>
