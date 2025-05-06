bbad031d072afea0e2ab0f25f29810ee72216b8e
FilipRosquist <filip.rosquist@gritacademy.se>
2025-05-06 18:41:28 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import About from './components/About'
 import Products from './components/Products'
 import Reviews from './components/Reviews'
 import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div w-full overflow-hidden>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Products/>
